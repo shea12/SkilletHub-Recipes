e38f0b4508a50df816f630d4202ecd99901a5f66
shea12 <cshearun12@gmail.com>
2016-09-28 12:36:22 -0700

    factor out authUser function, sign up and sign in are functional

--- controller.js
+++ controller.js
@@ -10,22 +10,39 @@ var USER_POOL_APP_CLIENT_ID = '3998t3ftof3q7k5f0cqn260smk';
 var USER_POOL_ID = 'us-west-2_P8tGz1Tx6';
 var COGNITO_IDENTITY_POOL_ID = 'us-west-2:ea2abcb1-10a0-4964-8c13-97067e5b50bb';
 
-AWS.config.region = 'us-west-2';
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-  IdentityPoolId: COGNITO_IDENTITY_POOL_ID
-});
-// AWS.config.update({accessKeyId: 'anything', secretAccessKey: 'anything'})
+
+/**************     AUTH HELPER FUNCTION    *****************
+************************************************************/
+var authorizeUser = function(authDeets, cognitoUser, callback) {
+  cognitoUser.authenticateUser(authDeets, {
+    onSuccess: function (result) {
+      var token = result.getAccessToken().getJwtToken();
+      // console.log('access token + ' + token); 
+      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId : COGNITO_IDENTITY_POOL_ID,
+        Logins : {
+          'cognito-idp.us-west-2.amazonaws.com/us-west-2_P8tGz1Tx6' : token
+        }
+      });
+      callback(null, token);
+    },
+    onFailure: function(err) {
+      console.log('Error in auth: ', err);
+      callback(err);
+    }
+  });
+}
+
+
 
 /***************    SIGN UP A NEW USER    *******************
 ************************************************************/
 exports.signup = function(req, cb) {
-  // Expecting req.body to contain:
-  //   username
-  //   password
-  //   firstName
-  //   lastName
-  //   email
-
+  AWS.config.region = 'us-west-2';
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: COGNITO_IDENTITY_POOL_ID
+  });
+  // AWS.config.update({accessKeyId: 'anything', secretAccessKey: 'anything'})
   var poolConfig = {
     UserPoolId: USER_POOL_ID,
     ClientId: USER_POOL_APP_CLIENT_ID
@@ -40,21 +57,18 @@ exports.signup = function(req, cb) {
   var lastname = {Name: 'family_name', Value: req.body.lastname};
   var email = {Name: 'email', Value: req.body.email};
   var timestamp = {Name: 'updated_at', Value: currentTime };
-  // var userId = {Name: 'userId', Value: req.body.userId};;
 
   var attPrefUsername = new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute(prefusername);
   var attFirstname = new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute(firstname);
   var attLastname = new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute(lastname);
   var attEmail = new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute(email);
   var attTimeStamp = new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute(timestamp);
-  // var attUserId = new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute(userId);
 
   attList.push(attPrefUsername);
   attList.push(attFirstname);
   attList.push(attLastname);
   attList.push(attEmail);
   attList.push(attTimeStamp);
-  // attList.push(attUserId);
 
   userPool.signUp(req.body.username, req.body.password, attList, null, function(error, result) {
     if (error) {
@@ -62,18 +76,23 @@ exports.signup = function(req, cb) {
       cb(error, null);
     } else {
       cognitoUser = result.user;
-      cognitoUser.confirmUser;
-      console.log('Sign up successful for user: ', cognitoUser);
-      cb(null, cognitoUser);
+
+      var authData = {
+        Username: req.body.username,
+        Password: req.body.password
+      };
+      var authDeets = new AWS.CognitoIdentityServiceProvider.AuthenticationDetails(authData);
+      
+      authorizeUser(authDeets, cognitoUser, function(err, token) {
+        if (err) {
+          console.log('Error authorizing, getting token', err);
+        } else {
+          cb(null, cognitoUser, token);    
+        }
+      });
     }
   });
 
-  // Need to begin session
-
-  // Expecting res to contain:
-  //   userId
-  //   session token
-  // cb(null, {message: 'signup response'});
 };
 
 
@@ -81,52 +100,31 @@ exports.signup = function(req, cb) {
 /**************    LOG IN EXISTING USER    ******************
 ************************************************************/
 exports.login = function(req, cb) {
-  // Expecting req.body to contain:
-  //   username
-  //   password
+  var userData = {
+    Username : req.body.username,
+    Pool : userPool
+  };
+  var cognitoUser = new AWS.CognitoIdentityServiceProvider.CognitoUser(userData);
+  
+  var poolConfig = {
+    UserPoolId: USER_POOL_ID,
+    ClientId: USER_POOL_APP_CLIENT_ID
+  };
+  var userPool = new AWS.CognitoIdentityServiceProvider.CognitoUserPool(poolConfig);
 
   var authData = {
     Username: req.body.username,
     Password: req.body.password
-  }
-
-  var authDeets = new AWS.CognitoIdentityServiceProvider.AuthenticationDetails(authData);
-  var poolConfig = {
-    UserPoolId: USER_POOL_ID,
-    ClientId: USER_POOL_APP_CLIENT_ID
-  }
-  var userPool = new AWS.CognitoIdentityServiceProvider.CognitoUserPool(poolConfig);
-  var userData = {
-      Username : req.body.username,
-      Pool : userPool
   };
-  var cognitoUser = new AWS.CognitoIdentityServiceProvider.CognitoUser(userData);
+  var authDeets = new AWS.CognitoIdentityServiceProvider.AuthenticationDetails(authData);
 
-  cognitoUser.authenticateUser(authDeets, {
-    onSuccess: function (result) {
-      console.log('access token + ' + result.getAccessToken().getJwtToken());
-
-      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-        IdentityPoolId : COGNITO_IDENTITY_POOL_ID,
-        Logins : {
-          'cognito-idp.us-west-2.amazonaws.com/us-west-2_P8tGz1Tx6' : result.getIdToken().getJwtToken()
-        }
-      });
-
-    // Instantiate aws sdk service objects now that the credentials have been updated.
-    // example: var s3 = new AWS.S3();
-    },
-    onFailure: function(err) {
-      console.log('Error in login: ', err);
-      cb(err);
+  authorizeUser(authDeets, cognitoUser, function(err, token) {
+    if (err) {
+      console.log('Error authorizing, getting token', err);
+    } else {
+      cb(null, cognitoUser, token);    
     }
   });
-
-
-  // Expecting res to contain:
-  //   userId
-  //   session token
-  // cb(null, {message: 'login response'});
 };
 
 
@@ -135,8 +133,6 @@ exports.login = function(req, cb) {
 /******************    LOG OUT USER    **********************
 ************************************************************/
 exports.logout = function(req, cb) {
-  // Expecting req.body to contain:
-  //   username
   var poolData = { 
     UserPoolId: USER_POOL_ID,
     ClientId: USER_POOL_APP_CLIENT_ID
@@ -150,7 +146,5 @@ exports.logout = function(req, cb) {
 
   cognitoUser.signOut();
 
-  // Expecting res to contain:
-  //   "logged out"
   cb(null, {message: 'logout response'});
 };
--- routes.js
+++ routes.js
@@ -2,23 +2,25 @@ var usersRouter = require('express').Router();
 var usersController = require('./controller.js');
 
 usersRouter.route('/signup').post(function(req, res) {
-  usersController.signup(req, function(error, data) {
+  usersController.signup(req, function(error, userData, token) {
   	if (error) {
   	  console.log('error in /user/signup route');
   	  res.send(400);
   	} else {
-  	  res.status(200).json(data);
+  	  console.log('token: ', token);
+  	  res.status(200).json(userData);
   	}
   });
 });
 
 usersRouter.route('/login').post(function(req, res) {
-  usersController.login(req, function(error, data) {
+  usersController.login(req, function(error, userData, token) {
   	if (error) {
   	  console.log('error in /user/login route');
   	  res.send(400);
   	} else {
-  	  res.status(200).json(data);
+  	  console.log('token: ', token);
+  	  res.status(200).json(userData);
   	}
   });
 });
